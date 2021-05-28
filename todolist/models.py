d74cdef493db2c2c16ee740c304da7e3443a75d0
Mohd. Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-05-29 04:05:14 +0600

    Committing after registering database

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class List(models.Model):
+    item=models.CharField(max_length=400)
+    date=models.DateField(('Date'),default=datetime.date.today)
+    completed=models.BooleanField(False)
+    
+    def __str__(self):
+        return self.item+" || "+str(self.date)+" || "+str(self.completed)
